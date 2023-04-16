f51387ad6fe27d58591d3d0e7f8a29791a602495
Jack Lanphear <35370647+newfoundlandknight@users.noreply.github.com>
2023-04-16 12:20:42 -0230

    fixing capital

--- send-email.php
+++ send-email.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link href="css/styles.css" rel="stylesheet" type="text/css" />
-    <title>Send Email</title>
+    <title>Send Email </title>
   </head>
   <body>
   <?php include 'menu-bar.php';?>
